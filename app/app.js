b2efdedd4b28f73ff789dbaac57b3396884d03c6
Victor Mattsson <victor.m.dravnieks@gmail.com>
2016-03-18 09:35:44 +0100

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('demoSite', [
+    'ngRoute',
+    'demoSite.search',
+    'demoSite.movie',
+    'demoSite.series',
+    'demoSite.season',
+    'demoSite.databaseService',
+    'demoSite.seasonService'
+]).
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+    otherwise({redirectTo: '/search'});
+}]);
+
+
